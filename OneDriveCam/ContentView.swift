dcee797ee96e9115f41628e1a6e11b57e0e7446a
Travis Brigman <travis1000@gmail.com>
2020-02-08 22:35:04 -0600

    Initial Commit

---
+++ OneDriveCam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OneDriveCam
+//
+//  Created by Travis Brigman on 2/8/20.
+//  Copyright © 2020 Travis Brigman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
